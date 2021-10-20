f5730a8fd3131ade9e03e4c4124dd30462229c74
masafumi-kadoi <masafumi.kadoi@woven-planet.global>
2021-10-20 16:39:08 +0900

    Instantiate The Gradebook

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,18 +40,15 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-
-// add below script
             var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
-            // BaseGradeBook gradeBook = new BaseGradeBook(name);
             else if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine("{0} is not a supported type of gradebook, plaese try again.", type);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
