6fc8cc87da7003b58abb5310d3da5c1a1c8e0a98
masafumi-kadoi <masafumi.kadoi@woven-planet.global>
2021-10-20 13:33:01 +0900

    fix Create GetLetterGrade Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,18 +13,18 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count <= 5)
+            if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-            var thr = (int)Math.Floor(Students.Count * 0.2);
+            var thr = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(a => a.AverageGrade).Select(a => a.AverageGrade).ToList();
 
-            if (grades[thr] <= averageGrade)
+            if (grades[thr -1] <= averageGrade)
                 return 'A';
-            else if (grades[thr * 2] <= averageGrade)
+            else if (grades[(thr * 2) - 1] <= averageGrade)
                 return 'B';
-            else if (grades[thr * 3] <= averageGrade)
+            else if (grades[(thr * 3) - 1] <= averageGrade)
                 return 'C';
-            else if (grades[thr * 4] <= averageGrade)
+            else if (grades[(thr * 4) - 1] <= averageGrade)
                 return 'D';
             else
                 return 'F';
